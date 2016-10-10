17c4c7686c54077fd01419b66086e261363ec74d
Jeffrey Kranking <jkranking@gmail.com>
2016-10-10 12:52:22 -0700

    Finish Day 21

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009162911) do
+ActiveRecord::Schema.define(version: 20161010172420) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
